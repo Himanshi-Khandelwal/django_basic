01e332221765d8628be01625cc9874221fcb83a1
himanshi <himanshikhandelwal8@gmail.com>
2017-05-12 15:18:35 +0530

    Add Requirements

--- tweety/wsgi.py
+++ tweety/wsgi.py
@@ -12,3 +12,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweety.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
