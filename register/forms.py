9e7cab520b1a28417d9fef20865da17ce0741910
himanshi <himanshikhandelwal8@gmail.com>
2017-05-13 22:24:25 +0530

    final

--- register/forms.py
+++ register/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from pagedown.widgets import PagedownWidget
+from django.contrib.auth import get_user_model
 from .models import images
+
+User = get_user_model()
 
 class SignUpForm(UserCreationForm):
     
