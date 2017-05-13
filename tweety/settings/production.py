f78bfa9ad85fc5f3319c34a48f4793be23d03e1c
himanshi <himanshikhandelwal8@gmail.com>
2017-05-13 18:57:07 +0530

    sign up changes

--- tweety/settings/production.py
+++ tweety/settings/production.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'tweety.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/var/www/dashboardapp/whatever/path/db.sqlite3',
     }
 }
 
